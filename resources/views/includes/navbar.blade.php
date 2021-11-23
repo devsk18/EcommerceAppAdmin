3d627e1488bb9872941c812e7e270da69f7cf7a0
devsk18 <kthampansam18@gmail.com>
2021-11-23 23:27:37 +0530

    product module added

--- resources/views/includes/navbar.blade.php
+++ resources/views/includes/navbar.blade.php
@@ -74,7 +74,18 @@
                           </p>
                       </a>
                   </li>
-               
+
+                  <li class="nav-header">Account</li>
+
+                  <li class="nav-item">
+                      <a href="{{ route('logout') }}" class="nav-link">
+                          <i class="nav-icon fas fa-sign-out-alt"></i>
+                          <p>
+                              Logout
+                          </p>
+                      </a>
+                  </li>
+
 
               </ul>
           </nav>
